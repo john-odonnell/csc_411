6671f41dd6153edb1e2860b81ef6975d3b14731d
John O'Donnell <johnodonnell77@homework.cs.uri.edu>
2020-02-05 23:47:25 -0500

    John O'Donnell: UArray2 made unboxed

--- assignment_2/uarray2.c
+++ assignment_2/uarray2.c
@@ -11,47 +11,28 @@ struct T{
 	int height;
 	int width;
 	int size;
-	char **array;
+	char *array;
 };
 
-void UArray2_init(T uarray2, int height, int width, int size, char **ary){
-	assert(uarray2);
-	assert(ary && height>0 || height==0 && ary==NULL);
-	assert(size>0);
+void UArray2_new(int height, int width, int size){
+	assert(height>0 && width >0 && size>0);
+
+	T uarray2;
+	NEW(uarray2);
 
 	uarray2->height = height;
 	uarray2->width = width;
 	uarray2->size = size;
 
-	if(height>0)
-		uarray2->array = ary;
-	else
-		uarray2->array = NULL;
+	uarray2->array = CALLOC(height*width, size);
 
-	for(int i=0;i<height;i++){
-		Array_T array;
-		array = Array_new(width, size);
-		*(uarray2->ary + sizeof(char *)*i) = &array;
-	}
-}
-
-void UArray2_new(int height, int width, int size){
-	T uarray2;
-	NEW(uarray2);
-	if(height>0 && width>0)
-		UArray2_init(uarray2, height, width, size, CALLOC(height, sizeof(char *)));
-	else
-		UArray2_init(uarray2, height, width, size, NULL);
-	return uarray2
+	return uarray2;
 }
 
 void UArray2_free(T *uarray2){
 	assert(uarray2 && *uarray2);
-	for(int i=0;i<height;i++){
-		Array_free(*((*uarray2)->array + i*uarray2->sizeof(char *)));
-	}
 	FREE((*uarray2)->array);
-	FREE(*uarray2);
+	FREE(uarray2);
 }
 
 int UArray2_height(T uarray2){
@@ -74,8 +55,22 @@ void *UArray2_at(T uarray2, int height, int width){
 	assert(height>=0 && height<uarray2->height);
 	assert(width>=0 && width<uarray2->width);
 	
-	return Array_get(**(uarray2->array + sizeof(char *)*height), width);
+	return ((uarray2->array + (uarray2->size * height)) + (uarray2->size * width));
 }
 
+void UArray_map_row_major(T uarray2, void apply(int n, int bit, void *cl), void *cl){
+	for(int i=0; i < (uarray2->height * uarray2->width); i++){
+		char *p = (uarray->array + (i * uarray2->size));
+		apply(p, *p, cl);
+	}
+}
 
+void UArray_map_col_major(T uarray2, void apply(int n, int bit, void *cl), void *cl){
+	for(int i = 0; i < uarray2->width; i++){
+		for(int j = 0; j < uarray2->height; j++){
+			char *p = ((uarray2->array + (uarray2->size * i)) + (uarray2->size * j));
+			apply(p, *p, cl);
+		}
+	}
+}
 
