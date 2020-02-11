f596517e36826009cc064b53e6ba015aab970013
John O'Donnell <johnodonnell77@homework.cs.uri.edu>
2020-02-10 22:54:50 -0500

    John O'Donnell: sudoku doesn't check boxes

--- assignment_2/sudoku.c
+++ assignment_2/sudoku.c
@@ -1,26 +1,80 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "mem.h"
 #include "assert.h"
 #include "uarray2.h"
 #include "pnmrdr.h"
+#include "set.h"
 
-void fill_uarray2(UArray2_T uarray2, Pnmrdr_T puzzle){
+//int cmp(const void *x, const void *y){
+//	if(*(int *)x > *(int *)y)
+//		return 1;
+//	else if(*(int *)x < *(int *)y)
+//		return -1;
+//	else
+//		return 0;
+//}
+//
+//unsigned hash(const void *x){
+//	return *(int *)x;
+//}
+
+void fill_uarray2(UArray2_T uarray2, Pnmrdr_T pnm){
 	int n;
 	int *r_location;
-	printf("insertion start\n");
-	printf("size: %d\n", uarray2->size);
 	for(int i=0; i<9; i++){
 		for(int j=0; j<9; j++){
-			n = Pnmrdr_get(puzzle);
+			n = Pnmrdr_get(pnm);
+			if(n<1 || n>9){
+				exit(1);
+			}
 			r_location = UArray2_at(uarray2, i, j);
 			*r_location = n;
 			printf("%d,%d: loc: %p\t%d\t%d\n", i, j, (void *)r_location, n, *r_location);
 		}
 	}
-	printf("insertion complete\n");
 	return;
+}
+
+bool check_rows(UArray2_T puzzle){
+	for(int i=0; i<9; i++){
+		for(int j=0; j<8; j++){
+			int *to_check = UArray2_at(puzzle, i, j);
+			for(int k=j+1; k<9; k++){
+				int *check_against = UArray2_at(puzzle, i, k);
+				if(*to_check == *check_against)
+					return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool check_columns(UArray2_T puzzle){
+	for(int i=0; i<9; i++){
+		for(int j=0; j<8; j++){
+			int *to_check = UArray2_at(puzzle, i, j);
+			for(int k=j+1; k<9; k++){
+				int *check_against = UArray2_at(puzzle, i, k);
+				if(*to_check == *check_against)
+					return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool check_solution(UArray2_T puzzle){
+	// Set_T *base_set;
+	// NEW(base_set);
+	// *base_set = Set_new(9, cmp, hash);
+
+	if(check_columns(puzzle) && check_rows(puzzle)) // && check_boxes(puzzle))
+		return true;
+	else
+		return false;
 }
 
 void apply_print(void *p, int bit, void *cl){
@@ -46,41 +100,42 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
-	Pnmrdr_T *puzzle;
-       	NEW(puzzle);
-	*puzzle = Pnmrdr_new(stream);
-	Pnmrdr_mapdata data = Pnmrdr_data(*puzzle);
+	Pnmrdr_T *pnm;
+       	NEW(pnm);
+	*pnm = Pnmrdr_new(stream);
+	Pnmrdr_mapdata data = Pnmrdr_data(*pnm);
 	assert(data.width==9 && data.height==9 && data.denominator==9);
 
-	UArray2_T *puzzle_R;
-	NEW(puzzle_R);
-	*puzzle_R = UArray2_new(9, 9, sizeof(int));
-	UArray2_map_row_major(*puzzle_R, apply_print, NULL);
-	
-	printf("filling array\n");
-	fill_uarray2(*puzzle_R, *puzzle);
-	printf("array filled\n");
+	UArray2_T *puzzle;
+	NEW(puzzle);
+	*puzzle = UArray2_new(9, 9, sizeof(int));
+		
+	fill_uarray2(*puzzle, *pnm);
+	Pnmrdr_free(pnm);
+	free(pnm);
 	fclose(stream);
 
 	for(int i=0; i<9; i++){
 		for(int j=0; j<9; j++){
-			int *val = UArray2_at(*puzzle_R, i, j);
+			int *val = UArray2_at(*puzzle, i, j);
 			printf("%d,%d: loc: %p\t%d\n", i, j, (void *)val, *val);
 		}
 	}
 		
-	UArray2_map_row_major(*puzzle_R, apply_print, NULL);
+	UArray2_map_row_major(*puzzle, apply_print, NULL);
 
-	//if (check_solution(*puzzle_R)){
-	//	exit(0);
-	//}
-	//else{
-	//	exit(1);
-	//}
-	
-	Pnmrdr_free(puzzle);
-	free(puzzle);
-	UArray2_free(puzzle_R);
-	free(puzzle_R);	
+	if (check_solution(*puzzle)){
+		UArray2_free(puzzle);
+		free(puzzle);
+		printf("Good Solution\n");
+		exit(0);
+	}
+	else{
+		UArray2_free(puzzle);
+		free(puzzle);
+		printf("Bad Solution\n");
+		exit(1);
+	}
+
 	return(0);
 }
